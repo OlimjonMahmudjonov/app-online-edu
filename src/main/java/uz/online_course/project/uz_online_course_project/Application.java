d87dea0ddcfdc51a47bebc5b3f949a1e16280ad5
Olimjon <devjava636.com>
2025-08-04 02:18:08 +0500

    all commit

--- src/main/java/uz/online_course/project/uz_online_course_project/Application.java
+++ src/main/java/uz/online_course/project/uz_online_course_project/Application.java
@@ -1,30 +1,15 @@
 package uz.online_course.project.uz_online_course_project;
 
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-import uz.online_course.project.uz_online_course_project.telegram.OnlineEdu;
 
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws TelegramApiException {
-        //SpringApplication.run(Application.class, args);
 
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-        OnlineEdu onlineEdu = context.getBean(OnlineEdu.class);
-
-
-        try {
-            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(onlineEdu);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
         }
     }
 
@@ -33,4 +18,4 @@ public class Application {
 
 
 
-}
+
